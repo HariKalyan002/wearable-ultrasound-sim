318a6a519d93e1cec728eb77b3fd915625ea06a1
HariKalyan002 <hlali002@ucr.edu>
2025-06-06 13:22:13 -0700

    Delete top/Wearable_modules directory

--- top/Wearable_modules/mux.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def mux_sensor_data(signal_params, strategy='static', user_selection=0):
-    selected_params = signal_params[user_selection]
-    return selected_params, None  
